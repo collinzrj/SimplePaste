71151c1e842e5c74389dee8eba1e45b81ed926e9
collin <collinzrj@outlook.com>
2021-09-12 20:52:00 -0400

    upload files

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var privateKey  = fs.readFileSync('privkey.pem', 'utf8');
-var certificate = fs.readFileSync('fullchain.pem', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
-const server = require('https').Server(credentials, app);
+var useHTTPS = true;
+var server;
+if (useHTTPS) {
+  var privateKey  = fs.readFileSync('privkey.pem', 'utf8');
+  var certificate = fs.readFileSync('fullchain.pem', 'utf8');
+  var credentials = {key: privateKey, cert: certificate};
+  server = require('https').Server(credentials, app);
+} else {
+  server = require('http').Server(app);
+}
 const io = require('socket.io')(server);
+
+const multer = require('multer');
 const port = process.env.PORT || 443;
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './files')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({ storage: storage});
 
 app.use(express.static(__dirname + '/'));
 // app.use(bodyParser.urlencoded({extend:true}));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.set('views', __dirname);
+app.use(express.static('files'))
 
 app.get('/', (req, res) => {
   var messages = [];
@@ -24,6 +42,18 @@ app.get('/', (req, res) => {
     messages = messages.slice(messages.length - 10, messages.length);
   }
   res.render('index', {items: messages})
+});
+
+app.post('/upload', upload.single('uploaded_file'), function (req, res, next) {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  console.log(file)
+  console.log("test")
+  res.redirect('/')
 });
 
 io.on('connection', (socket) => {
